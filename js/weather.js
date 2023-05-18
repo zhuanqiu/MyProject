0d3099d90a415046a4faf41cac3ca84742e77a46
zhuanqiu <zhuanqiu84@gmail.com>
2023-05-18 15:50:26 +0800

    修改

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,7 @@
 $(function() {
     const cors = 'https://cors-anywhere.herokuapp.com/';
     $.ajax({
-        url: cors + 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7&locationName=%E4%B8%AD%E6%AD%A3%E5%8D%80&elementName=T',
+        url: 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-063?Authorization=CWB-A13D4A9A-CCDC-492F-B370-A3E7289AE3C7&locationName=%E4%B8%AD%E6%AD%A3%E5%8D%80&elementName=T',
         type: "GET",
         dataType: "json",
         success: function(res) {
